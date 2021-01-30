30c475fbef82e67dc8e96825a42b763ef8fd8f2d
= <=pablo_omesquita@hotmail.com>
2021-01-30 18:49:52 -0300

    Update

--- posts/views.py
+++ posts/views.py
@@ -2,21 +2,49 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.edit import UpdateView
 from posts.models import Post
+from django.db.models import Q, Count, Case, When
 # Create your views here.
 
 
 class PostIndex(ListView):
     model = Post
     template_name = 'posts/index.html'
-    paginate_by = 1
+    paginate_by = 6
+
     context_object_name = 'posts'
     
+    def get_queryset(self):
+        qs = super().get_queryset()
+        qs = qs.order_by('-id').filter(publicado_post=True)
+        qs = qs.annotate(
+            numero_comentarios = Count(
+                Case(
+                    When(comentario__publicado_comentario = True, then = 1)
+                )
+            )
+        )
+        return qs
+
 
 class PostBusca(PostIndex):
-    pass
+    template_name = 'posts/post_busca.html'
 
 class PostCategoria(PostIndex):
-    pass
+    template_name = 'posts/post_categoria.html'
+    
+    def get_queryset(self):
+        qs = super().get_queryset()
+        categoria = self.kwargs.get('categoria', None)
+
+        if not categoria:
+            return qs
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)
+
+        return qs
+
+
+
+
 
 class PostDetalhes(UpdateView):
     pass
